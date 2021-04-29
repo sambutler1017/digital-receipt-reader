410d5003248dce1117dd1912b1a6154dd6155440
Samuel Butler <sambutler1017@icloud.com>
2021-04-29 13:08:27 -0400

    Adding Loading Indicator

---
+++ DigtailReceiptReader/DigtailReceiptReader/lib/LoadingIndicator.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DigtailReceiptReader.lib {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoadingIndicator : ContentView {
+        public readonly static BindableProperty IsLoadingProperty    = BindableProperty.Create("IsLoading", typeof(bool), typeof(LoadingIndicator), false);
+        public readonly static BindableProperty LoadingColorProperty = BindableProperty.Create("LoadingColor", typeof(string), typeof(LoadingIndicator), "Orange");
+
+        public LoadingIndicator() {
+            InitializeComponent();
+        }
+
+        public bool IsLoading {
+            get => (bool)GetValue(IsLoadingProperty);
+            set => SetValue(IsLoadingProperty, value);
+        }
+
+        public string LoadingColor {
+            get => (string)GetValue(LoadingColorProperty);
+            set => SetValue(LoadingColorProperty, value);
+        }
+    }
+}
