65372cde167af2fdf7ae690b4e3ed2bb9e222fe8
Samuel Butler <sambutler1017@icloud.com>
2021-05-04 10:38:19 -0400

    no message

--- DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/MainActivity.java
+++ DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/MainActivity.java
@@ -20,11 +20,11 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home,
+                R.id.navigation_dashboard, R.id.navigation_notifications).build();
 
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
