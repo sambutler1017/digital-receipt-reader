2c10ce46033ba1f77ce58c327d77b05d7e1ce071
Samuel Butler <sambutler1017@icloud.com>
2021-07-27 21:16:17 -0400

    Adding button listener

--- DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/LoginActivity.java
+++ DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/LoginActivity.java
@@ -2,13 +2,41 @@ package com.ridge.digitalreceiptreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailInput;
+    private EditText passwordInput;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailInput = (EditText)findViewById(R.id.email_textbox__login);
+        passwordInput = (EditText) findViewById(R.id.password_textbox__login);
+        loginButton = (Button)findViewById(R.id.login_button__login);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLogin(emailInput.getText().toString(), passwordInput.getText().toString());
+            }
+        });
+    }
+
+    private void onLogin(String email, String password) {
+        if(email.equals("test") && password.equals("password")) {
+           Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+           startActivity(intent);
+        } else {
+            loginButton.setText("Invalid Credentials");
+        }
     }
 }
--- DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/MainActivity.java
+++ DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/MainActivity.java
@@ -17,16 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home,
-                R.id.navigation_dashboard, R.id.navigation_notifications).build();
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.nav_host_fragment);
         NavController navController = navHostFragment.getNavController();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
-
 }
