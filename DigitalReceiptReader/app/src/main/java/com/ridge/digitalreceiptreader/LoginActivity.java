652d8050cc4eb2fb735a68f6d82d9c0f315a7e81
Samuel Butler <sambutler1017@icloud.com>
2021-07-28 17:35:45 -0400

    no message

--- DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/LoginActivity.java
+++ DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/LoginActivity.java
@@ -4,17 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.ridge.digitalreceiptreader.service.toast.ToastService;
 
 public class LoginActivity extends AppCompatActivity {
+    private ToastService toastService;
 
     private EditText emailInput;
     private EditText passwordInput;
     private Button loginButton;
+    private ProgressBar loadingIndicator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,9 @@ public class LoginActivity extends AppCompatActivity {
         emailInput = (EditText)findViewById(R.id.email_textbox__login);
         passwordInput = (EditText) findViewById(R.id.password_textbox__login);
         loginButton = (Button)findViewById(R.id.login_button__login);
+        toastService = new ToastService(this);
+        loadingIndicator = (ProgressBar)findViewById(R.id.loading_indicator__login);
+        loadingIndicator.setVisibility(View.GONE);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,12 +42,19 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void onLogin(String email, String password) {
-        ToastService toastService = new ToastService(this);
-
         if(email.equals("test") && password.equals("password")) {
-           Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-           startActivity(intent);
-            toastService.showSuccess("Logged in Successfully!");
+            loadingIndicator.setVisibility(View.VISIBLE);
+            loginButton.setVisibility(View.GONE);
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    toastService.showSuccess("Logged in Successfully!");
+                }
+            }, 2000);
+
         } else {
             toastService.showError("Invalid Credentials!");
         }
