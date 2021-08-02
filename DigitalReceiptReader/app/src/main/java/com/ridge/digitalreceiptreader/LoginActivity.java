559c35def24b8c9907f4dcbba76b6d35ba67f2ca
Samuel Butler <sambutler1017@icloud.com>
2021-08-02 18:36:44 -0400

    no message

--- DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/LoginActivity.java
+++ DigitalReceiptReader/app/src/main/java/com/ridge/digitalreceiptreader/LoginActivity.java
@@ -35,6 +35,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        initialization();
+    }
+
+    private void initialization() {
         initClients();
         initServices();
         initElements();
