df5e5553f184e633add47fb8f49fda60585f7200
Sam Butler <67570772+sambutler1017@users.noreply.github.com>
2021-10-29 16:48:25 -0400

    Task/rcsd 40 finish up user endpoint (#9)
    * no message
    
    * no message

--- app/src/main/java/com/ridge/digitalreceiptreader/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ridge/digitalreceiptreader/ui/profile/ProfileFragment.java
@@ -21,8 +21,6 @@ public class ProfileFragment extends Fragment {
         profileViewModel =
                 new ViewModelProvider(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
         return root;
     }
 }
